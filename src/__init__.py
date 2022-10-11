852042e5b66379995a357bf124c58ad75ba18a0a
nthoan3 <nthoan3@cmcglobal.vn>
2022-10-11 09:44:07 +0700

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from flask.json import jsonify
+from src.detect import detect
+
+def create_app(test_config=None):
+    
+    app = Flask(__name__, instance_relative_config=True)
+    
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+        )
+    else:
+        app.config.from_mapping(test_config)
+
+    app.register_blueprint(detect)
+    
+    @app.errorhandler(HTTP_404_NOT_FOUND)
+    def handle_404(e):
+        return jsonify({'error': "Not found"}), HTTP_404_NOT_FOUND
+    
+    @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handle_500(e):
+        return jsonify({'error': "Something went wrong, we are working on it"}), HTTP_500_INTERNAL_SERVER_ERROR
+
+    return app
---
+++ src/detect.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request, jsonify
+from craft_text_detector import Craft
+from src.constants.http_status_codes import HTTP_200_OK
+import numpy as np
+from PIL import Image
+import requests
+import json
+
+detect = Blueprint("detect", __name__, url_prefix="/api/v1/detect")
+
+long_size = 1280
+craft = Craft(output_dir=None, crop_type="box",
+              cuda=False, long_size=long_size)
+
+
+class Images:
+    def __init__(self, url, boxes):
+        self.url = url
+        self.boxes = boxes
+        self.size = long_size
+
+
+@detect.get('/')
+def detect_images():
+    images = request.json["images"]
+
+    result = []
+    for url in images:
+        response = requests.get(url, stream=True)
+        image = np.array(Image.open(response.raw))
+        prediction_result = craft.detect_text(image)
+        boxes = prediction_result["boxes"].tolist()
+        img = Images(url, boxes)
+        result.append(img.__dict__)
+
+    return jsonify({
+        'message': "Image detect successfully!",
+        "images": result
+    }), HTTP_200_OK
